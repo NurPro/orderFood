a2ae2fee53abae350873acc1d6bbea7eb131dddd
NurPro <uchdobo04@gmail.com>
2023-05-03 11:31:12 +0600

    order

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/NurPro/orderFood.git
+git push -u origin main
+ */
